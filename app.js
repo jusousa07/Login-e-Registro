49723b852872fdba4b873073c1fb4cd04cae5215
judson sousa <142075881+jusousa07@users.noreply.github.com>
2025-03-17 02:52:29 -0300

    criando password e usuario

--- app.js
+++ app.js
@@ -41,6 +41,26 @@ app.post('/auth/register', async (req, res) => {
         return res.status(422).json({ msg: 'Por favor, utilize outro email!' })
     }
 
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password,salt)
+
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    })
+
+    try {
+        await user.save()
+
+      res.status(201).json({ msg: 'Usuário criado com sucesso! ' })  
+    } catch (error)  {
+        console.log(error)
+
+        res.status(500).json({
+                msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!',
+            })
+    }
 });
 
 
